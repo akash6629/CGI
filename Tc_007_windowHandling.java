5a2d6974ffa986f034a3ebcf556e2662dbfcaa9e
Akash <2110173@reva.edu.in>
2025-08-25 16:17:31 +0530

    updated

--- Tc_007_windowHandling.java
+++ Tc_007_windowHandling.java
@@ -6,11 +6,8 @@ import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import java.time.Duration;
 
 public class Tc_007_windowHandling {
 
@@ -18,7 +15,8 @@ public class Tc_007_windowHandling {
         // Setup ChromeDriver
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+
 
         driver.get("https://letcode.in/window");
         System.out.println("window handle: " + driver.getWindowHandle());
@@ -37,27 +35,23 @@ public class Tc_007_windowHandling {
 
             if (driver.getCurrentUrl().equals("https://letcode.in/alert")) {
 
-                // ===== Simple Alert =====
                 driver.findElement(By.id("accept")).click();
-                Alert simpleAlert = wait.until(ExpectedConditions.alertIsPresent()); // wait for alert
+                Alert simpleAlert = driver.switchTo().alert();
                 System.out.println("Simple Alert text: " + simpleAlert.getText());
                 simpleAlert.accept(); // click OK
-                wait.until(ExpectedConditions.not(ExpectedConditions.alertIsPresent())); // wait until closed
 
-                // ===== Confirm Alert =====
+              
                 driver.findElement(By.id("confirm")).click();
-                Alert confirmAlert = wait.until(ExpectedConditions.alertIsPresent());
+                Alert confirmAlert = driver.switchTo().alert();
                 System.out.println("Confirm Alert text: " + confirmAlert.getText());
                 confirmAlert.dismiss(); // click Cancel
-                wait.until(ExpectedConditions.not(ExpectedConditions.alertIsPresent())); // wait until closed
 
-                // ===== Prompt Alert =====
+            
                 driver.findElement(By.id("prompt")).click();
-                Alert promptAlert = wait.until(ExpectedConditions.alertIsPresent());
+                Alert promptAlert = driver.switchTo().alert();
                 System.out.println("Prompt Alert text: " + promptAlert.getText());
                 promptAlert.sendKeys("Akash"); // enter text in prompt
                 promptAlert.accept(); // click OK
-                wait.until(ExpectedConditions.not(ExpectedConditions.alertIsPresent())); // wait until closed
             }
         }
 
